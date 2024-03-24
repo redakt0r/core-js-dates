603df50e2632023691339bef7de34e638ab53031
Mikhail Koryakin <sdrr87@gmail.com>
2024-03-24 20:54:21 +0300

    feat: add getDayName solution

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -46,8 +46,17 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const weekDays = {
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    0: 'Sunday',
+  };
+  return weekDays[new Date(date).getDay()];
 }
 
 /**
